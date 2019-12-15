ed7e6bd5b7f7f1a713598cf9a5b4273e0a4a2037
Laura Campa <lauras_here@pm.me>
2019-12-15 09:54:15 -0800

    Continue UI redesign

--- static/assets/js/menutoggle.js
+++ static/assets/js/menutoggle.js
@@ -18,3 +18,53 @@ navToggle.addEventListener('click', () => {
        navToggleIcon.item(1).classList.replace('fa-window-close', 'fa-bars');
    }
 });
+
+ function animateWhenVisible() {
+    var targets = document.querySelectorAll('.animateWhenVisible');
+    var intersectionObserverOptions = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.1
+    };
+    var observer = new IntersectionObserver(onIntersection, intersectionObserverOptions);
+    targets.forEach(q => {
+      observer.observe(q);
+    });
+    function onIntersection(entries) {
+      entries.forEach(entry => {
+        if (entry.intersectionRatio > 0) {
+          entry.target.classList.add('animated', 'fadeInUpSmall');
+        }
+        if (entry.intersectionRatio > 0) {
+          observer.unobserve(entry.target);
+        }
+      });
+    }
+  }
+  animateWhenVisible();
+ 
+  /*
+  function lazyLoad() {
+    var targets = document.querySelectorAll('.lazy-load');
+    var intersectionObserverOptions = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.1
+    };
+    var observer = new IntersectionObserver(onIntersection, intersectionObserverOptions);
+    targets.forEach(q => {
+      observer.observe(q);
+    });
+    function onIntersection(entries) {
+      entries.forEach(entry => {
+        if (entry.intersectionRatio > 0) {
+          entry.target.classList.add('animated', 'fadeInUpSmall');
+        }
+        if (entry.intersectionRatio > 0) {
+          observer.unobserve(entry.target);
+        }
+      });
+    }
+  }
+  lazyLoad();
+  */

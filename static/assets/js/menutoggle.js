f928098985111f83d3769b87c5108db359ec4b57
Laura Campa <lauras_here@pm.me>
2020-01-08 19:49:33 -0800

    Change nav classes and nav script

--- static/assets/js/menutoggle.js
+++ static/assets/js/menutoggle.js
@@ -1,70 +1,86 @@
-const navToggle = document.querySelector('.nav-toggle-icon');
-const html = document.querySelector('html');
-const navContent = document.querySelector('.nav-content');
-const navToggleIcon = navToggle.children;
-const menuToggle = document.querySelectorAll('.menu-toggle-icon');
+let nav = document.querySelector('.nav');
+let navContent = document.querySelector('.nav__content');
 
-document.addEventListener('DOMContentLoaded', () => {
-    html.classList.toggle('js');
-});
 
+navContent.hidden = true;
+
+
+
+const btn = document.createElement("button");
+
+btn.classList.add("nav__toggle");
+btn.setAttribute('aria-expanded', 'false');
+btn.innerHTML = `
+<i class="fas fa-2x fa-bars"></i>
+`
+
+btn.addEventListener('click', (e) => {
+  navContent.classList.toggle('show')  
+}) 
+
+if (nav) {
+  nav.insertBefore(btn, navContent);
+}
+
+/*
 navToggle.addEventListener('click', () => {
-   navContent.classList.toggle('show'); 
-   if (navContent.classList.contains('show')) {
-       navToggleIcon.item(0).classList.replace('fa-bars', 'fa-window-close');
-       navToggle.setAttribute('aria-expanded', 'true');
-   } else {
-       navToggleIcon.item(0).classList.replace('fa-window-close', 'fa-bars');
-       navToggle.setAttribute('aria-expanded', 'false');
-   }
+  navContent.classList.toggle('show'); 
+  if (navContent.classList.contains('show')) {
+    navToggleIcon.item(0).classList.replace('fa-bars', 'fa-window-close');
+    navToggle.setAttribute('aria-expanded', 'true');
+  } else {
+    navToggleIcon.item(0).classList.replace('fa-window-close', 'fa-bars');
+    navToggle.setAttribute('aria-expanded', 'false');
+  }
 });
+*/
 
- function animateWhenVisible() {
-    var targets = document.querySelectorAll('.animateWhenVisible');
-    var intersectionObserverOptions = {
-      root: null,
-      rootMargin: '0px',
-      threshold: 0.1
-    };
-    var observer = new IntersectionObserver(onIntersection, intersectionObserverOptions);
-    targets.forEach(q => {
-      observer.observe(q);
+function animateWhenVisible() {
+  var targets = document.querySelectorAll('.animateWhenVisible');
+  var intersectionObserverOptions = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.1
+  };
+  var observer = new IntersectionObserver(onIntersection, intersectionObserverOptions);
+  targets.forEach(q => {
+    observer.observe(q);
+  });
+  function onIntersection(entries) {
+    entries.forEach(entry => {
+      if (entry.intersectionRatio > 0) {
+        entry.target.classList.add('animated', 'fadeInUpSmall');
+      }
+      if (entry.intersectionRatio > 0) {
+        observer.unobserve(entry.target);
+      }
     });
-    function onIntersection(entries) {
-      entries.forEach(entry => {
-        if (entry.intersectionRatio > 0) {
-          entry.target.classList.add('animated', 'fadeInUpSmall');
-        }
-        if (entry.intersectionRatio > 0) {
-          observer.unobserve(entry.target);
-        }
-      });
-    }
   }
-  animateWhenVisible();
- 
-  /*
-  function lazyLoad() {
-    var targets = document.querySelectorAll('.lazy-load');
-    var intersectionObserverOptions = {
-      root: null,
-      rootMargin: '0px',
-      threshold: 0.1
-    };
-    var observer = new IntersectionObserver(onIntersection, intersectionObserverOptions);
-    targets.forEach(q => {
-      observer.observe(q);
+}
+animateWhenVisible();
+
+/*
+function lazyLoad() {
+  var targets = document.querySelectorAll('.lazy-load');
+  var intersectionObserverOptions = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.1
+  };
+  var observer = new IntersectionObserver(onIntersection, intersectionObserverOptions);
+  targets.forEach(q => {
+    observer.observe(q);
+  });
+  function onIntersection(entries) {
+    entries.forEach(entry => {
+      if (entry.intersectionRatio > 0) {
+        entry.target.classList.add('animated', 'fadeInUpSmall');
+      }
+      if (entry.intersectionRatio > 0) {
+        observer.unobserve(entry.target);
+      }
     });
-    function onIntersection(entries) {
-      entries.forEach(entry => {
-        if (entry.intersectionRatio > 0) {
-          entry.target.classList.add('animated', 'fadeInUpSmall');
-        }
-        if (entry.intersectionRatio > 0) {
-          observer.unobserve(entry.target);
-        }
-      });
-    }
   }
-  lazyLoad();
-  */
+}
+lazyLoad();
+*/
